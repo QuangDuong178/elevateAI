be75880b912e1885458d175bd81e30767b9e1e9f
QuangDương <minhquang.17820@gmail.com>
2023-10-13 01:04:23 +0700

    update night

--- src/components/organisms/Navigator/index.tsx
+++ src/components/organisms/Navigator/index.tsx
@@ -1,18 +1,20 @@
-import {Layout, Menu} from "antd";
+import { Layout, Menu } from 'antd';
+import { useNavigator } from '@/composables/useNavigator.tsx';
+import "./style.scss"
 
 export const Navigator = () => {
-    const {Sider} = Layout;
-    // const {menuItems} = useNavigator();
-    return (
-        <Sider className={"navigator-organism"} collapsible width={300}>
-            <div className={"demo-logo-vertical"}></div>
-            <Menu
-                theme="dark"
-                mode="inline"
-                defaultSelectedKeys={['1']}
-                items={menuItems}
-            />
+  const { Sider } = Layout;
+  const { menuItems } = useNavigator();
+  return (
+    <Sider className={'navigator-organism'} collapsible width={300}>
+      <div className={'demo-logo-vertical'}></div>
+      <Menu
+        theme='dark'
+        mode='inline'
+        defaultSelectedKeys={['1']}
+        items={menuItems}
+      />
 
-        </Sider>
-    )
-}
+    </Sider>
+  );
+};
